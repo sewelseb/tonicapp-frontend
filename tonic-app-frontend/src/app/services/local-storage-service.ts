f1994fc637db23987f7c19d4622109002a549c0e
Sébastien de Beauffort <sebastien.debeauffort@vinci.be>
2023-02-23 12:16:33 +0100

    add - retrieve access token from local memory

--- tonic-app-frontend/src/app/services/local-storage-service.ts
+++ tonic-app-frontend/src/app/services/local-storage-service.ts
@@ -7,5 +7,12 @@ export class LocalStorageService {
     saveToken(token: string) {
         localStorage.setItem("token", token);
     }
+
+    getToken(): string {
+        let token = localStorage.getItem("token");
+        if(token === null) return"";
+
+        return token;
+    }
     
 }
